020b2828087ada106aa03cd09c9ec3a1e6b10229
Sathvik <88944590+User319183@users.noreply.github.com>
2023-12-02 09:35:35 -0500

    Update nuker.go

--- nuker.go
+++ nuker.go
@@ -17,28 +17,30 @@ import (
     "time"
 )
 
-// getProxy selects a random proxy from a list in a file
-func getProxy() string {
+var proxies []string
+
+func init() {
     data, _ := ioutil.ReadFile("proxies.txt")
-    proxies := strings.Split(string(data), "\n")
+    proxies = strings.Split(string(data), "\n")
+}
+
+func getProxy() string {
     return proxies[rand.Intn(len(proxies))]
 }
 
-// setTitle sets the title of the console window
 func setTitle(title string) {
     cmd := exec.Command("cmd", "/c", "title "+title)
     cmd.Stdout = os.Stdout
     cmd.Run()
 }
 
-// spam sends messages to a webhook, retrying on failure. It also prints the number of successes, failures, and errors.
-func spam(webhook string, msg string, sleep time.Duration, wg *sync.WaitGroup, success *int, fail *int, errors *int) {
+func spam(webhook string, msg string, tts bool, sleep time.Duration, wg *sync.WaitGroup, success *int, fail *int, errors *int) {
     defer wg.Done()
     for {
         proxy := getProxy()
         proxyUrl, _ := url.Parse("http://" + proxy)
 
-        msgMap := map[string]string{"content": msg}
+        msgMap := map[string]interface{}{"content": msg, "tts": tts}
         msgJson, _ := json.Marshal(msgMap)
 
         proxyReq, _ := http.NewRequest("POST", webhook, bytes.NewBuffer(msgJson))
@@ -49,24 +51,20 @@ func spam(webhook string, msg string, sleep time.Duration, wg *sync.WaitGroup, s
                 Proxy: http.ProxyURL(proxyUrl),
             },
         }
-        var resp *http.Response
-        var err error
-        for i := 0; i < 3; i++ {
-            resp, err = client.Do(proxyReq)
-            if err == nil {
-                break
-            }
-            time.Sleep(time.Second * time.Duration(i+1))
-        }
+        resp, err := client.Do(proxyReq)
         if err != nil {
             *errors++
-			fmt.Println("\033[31mError | " + err.Error() + "\033[0m")
+            fmt.Println("\033[31mError | " + err.Error() + "\033[0m")
         } else if resp.StatusCode == 204 {
             *success++
-			fmt.Println("\033[32mSent Message | " + resp.Status + "\033[0m")
+            fmt.Println("\033[32mSent Message | " + resp.Status + "\033[0m")
+        } else if resp.StatusCode == 429 {
+            retryAfter, _ := strconv.Atoi(resp.Header.Get("Retry-After"))
+            fmt.Println("\033[33mRate limited. Sleeping for " + strconv.Itoa(retryAfter) + "ms\033[0m")
+            time.Sleep(time.Duration(retryAfter) * time.Millisecond)
         } else {
             *fail++
-			fmt.Println("\033[31mFailed To Send Message | " + resp.Status + "\033[0m")
+            fmt.Println("\033[31mFailed To Send Message | " + resp.Status + "\033[0m")
         }
         setTitle(fmt.Sprintf("Success: %d, Fail: %d, Errors: %d", *success, *fail, *errors))
         time.Sleep(sleep)
@@ -102,21 +100,25 @@ func main() {
         }
         webhooks = append(webhooks, webhook)
     }
-	fmt.Print("\033[31mPlease Insert Message: \033[0m")
-	msg, _ := reader.ReadString('\n')
-	fmt.Print("\033[31mPlease Insert Threads (It's Recommended To Use 10): \033[0m")
-	threadsStr, _ := reader.ReadString('\n')
-	threads, _ := strconv.Atoi(strings.TrimSpace(threadsStr))
-	fmt.Print("\033[31mPlease Insert Sleep (It's Recommended To Use 2): \033[0m")
-	sleepStr, _ := reader.ReadString('\n')
-	sleep, _ := strconv.Atoi(strings.TrimSpace(sleepStr))
-	fmt.Println("\033[31mStarting...\033[0m")
-	var wg sync.WaitGroup
-	success, fail, errors := 0, 0, 0
+    fmt.Print("\033[31mPlease Insert Message: \033[0m")
+    msg, _ := reader.ReadString('\n')
+
+    fmt.Print("\033[31mDo you want to send TTS messages? (yes/no): \033[0m")
+    ttsStr, _ := reader.ReadString('\n')
+    tts := strings.TrimSpace(ttsStr) == "yes"
+    fmt.Print("\033[31mPlease Insert Threads (It's Recommended To Use 10): \033[0m")
+    threadsStr, _ := reader.ReadString('\n')
+    threads, _ := strconv.Atoi(strings.TrimSpace(threadsStr))
+    fmt.Print("\033[31mPlease Insert Sleep (It's Recommended To Use 2): \033[0m")
+    sleepStr, _ := reader.ReadString('\n')
+    sleep, _ := strconv.Atoi(strings.TrimSpace(sleepStr))
+    fmt.Println("\033[31mStarting...\033[0m")
+    var wg sync.WaitGroup
+    success, fail, errors := 0, 0, 0
     for i := 0; i < threads; i++ {
         for _, webhook := range webhooks {
             wg.Add(1)
-            go spam(webhook, strings.TrimSpace(msg), time.Duration(sleep)*time.Second, &wg, &success, &fail, &errors)
+            go spam(webhook, strings.TrimSpace(msg), tts, time.Duration(sleep)*time.Second, &wg, &success, &fail, &errors)
         }
     }
     wg.Wait()
