af139176e61f37df2147606eb5b0eddd25a0f331
Sathvik <88944590+User319183@users.noreply.github.com>
2023-12-02 20:42:11 -0500

    Update nuker.go

--- nuker.go
+++ nuker.go
@@ -18,6 +18,7 @@ import (
 )
 
 var proxies []string
+var startTime time.Time
 
 func init() {
     data, _ := ioutil.ReadFile("proxies.txt")
@@ -34,13 +35,19 @@ func setTitle(title string) {
     cmd.Run()
 }
 
-func spam(webhook string, msg string, tts bool, sleep time.Duration, wg *sync.WaitGroup, success *int, fail *int, errors *int) {
+func spam(webhook string, msg string, tts bool, username string, avatarURL string, sleep time.Duration, wg *sync.WaitGroup, success *int, fail *int, errors *int) {
     defer wg.Done()
     for {
         proxy := getProxy()
         proxyUrl, _ := url.Parse("http://" + proxy)
 
         msgMap := map[string]interface{}{"content": msg, "tts": tts}
+        if username != "" {
+            msgMap["username"] = username
+        }
+        if avatarURL != "" {
+            msgMap["avatar_url"] = avatarURL
+        }
         msgJson, _ := json.Marshal(msgMap)
 
         proxyReq, _ := http.NewRequest("POST", webhook, bytes.NewBuffer(msgJson))
@@ -66,7 +73,12 @@ func spam(webhook string, msg string, tts bool, sleep time.Duration, wg *sync.Wa
             *fail++
             fmt.Println("\033[31mFailed To Send Message | " + resp.Status + "\033[0m")
         }
-        setTitle(fmt.Sprintf("Success: %d, Fail: %d, Errors: %d", *success, *fail, *errors))
+        total := *success + *fail + *errors
+        percent := float64(*success) / float64(total) * 100
+        elapsed := time.Since(startTime).Minutes()
+        rate := float64(*success) / elapsed
+        rateRounded := fmt.Sprintf("%.1f", rate)
+        setTitle(fmt.Sprintf("Success: %d, Fail: %d, Errors: %d, Success Rate: %s per minute @ %.2f%%, Elapsed Time: %.2f minutes", *success, *fail, *errors, rateRounded, percent, elapsed))
         time.Sleep(sleep)
     }
 }
@@ -86,7 +98,7 @@ func main() {
                                                                                                 
     ╔═══════════════════════════════════════════════╗
     ║ User319183 | discord.gg/KHJjX3y2B4            ║
-    ║ Discord Webhook Spammer                       ║
+    ║ The fastest Discord Webhook Spammer           ║
     ╚═══════════════════════════════════════════════╝
     ` + "\033[0m")
     reader := bufio.NewReader(os.Stdin)
@@ -100,26 +112,50 @@ func main() {
         }
         webhooks = append(webhooks, webhook)
     }
+
+    fmt.Print("\033[31mPlease Insert Custom Username (press enter to skip): \033[0m")
+    username, _ := reader.ReadString('\n')
+    username = strings.TrimSpace(username)
+
+    fmt.Print("\033[31mPlease Insert Avatar URL (press enter to skip): \033[0m")
+    avatarURL, _ := reader.ReadString('\n')
+    avatarURL = strings.TrimSpace(avatarURL)
+    
     fmt.Print("\033[31mPlease Insert Message: \033[0m")
     msg, _ := reader.ReadString('\n')
 
     fmt.Print("\033[31mDo you want to send TTS messages? (yes/no): \033[0m")
     ttsStr, _ := reader.ReadString('\n')
     tts := strings.TrimSpace(ttsStr) == "yes"
-    fmt.Print("\033[31mPlease Insert Threads (It's Recommended To Use 10): \033[0m")
+    fmt.Print("\033[31mPlease Insert Threads (It's Recommended To Use 1): \033[0m")
     threadsStr, _ := reader.ReadString('\n')
     threads, _ := strconv.Atoi(strings.TrimSpace(threadsStr))
-    fmt.Print("\033[31mPlease Insert Sleep (It's Recommended To Use 2): \033[0m")
+    fmt.Print("\033[31mPlease Insert Sleep in seconds (It's Recommended To Use 1.2): \033[0m")
     sleepStr, _ := reader.ReadString('\n')
     sleep, _ := strconv.Atoi(strings.TrimSpace(sleepStr))
     fmt.Println("\033[31mStarting...\033[0m")
+    startTime = time.Now()
     var wg sync.WaitGroup
     success, fail, errors := 0, 0, 0
     for i := 0; i < threads; i++ {
         for _, webhook := range webhooks {
             wg.Add(1)
-            go spam(webhook, strings.TrimSpace(msg), tts, time.Duration(sleep)*time.Second, &wg, &success, &fail, &errors)
+            go spam(webhook, strings.TrimSpace(msg), tts, username, avatarURL, time.Duration(sleep)*time.Second, &wg, &success, &fail, &errors)
         }
     }
+
+    // New goroutine to update the title every second
+    go func() {
+        for {
+            total := success + fail + errors
+            percent := float64(success) / float64(total) * 100
+            elapsed := time.Since(startTime).Minutes()
+            rate := float64(success) / elapsed
+            rateRounded := fmt.Sprintf("%.1f", rate)
+            setTitle(fmt.Sprintf("Success: %d, Fail: %d, Errors: %d, Success Rate: %s per minute @ %.2f%%, Elapsed Time: %.2f minutes", success, fail, errors, rateRounded, percent, elapsed))
+            time.Sleep(1 * time.Second)
+        }
+    }()
+
     wg.Wait()
 }
